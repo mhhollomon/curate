9478c2c6879e6964cad1bced80084cb24cf424f1
Mark Hollomon <mhhollomon@gmail.com>
2025-12-11 16:02:01 -0500

    start of api

--- README.md
+++ README.md
@@ -2,10 +2,25 @@
 
 Fun DYI project to stream music
 
+# Server API
+
+## /api/artist
+List all artists
+
+## /api/artist/<id>
+List music (singles and albums) for that artist
+
 
 # Development
 
 ## Run gen_data
 ```bash
 python -m src.Curate.gen_data --music /mnt/music/Bandcamp --out output
+```
+
+## Run the server in debug mode
+```bash
+# listens on http://localhost:9999
+#expects the database to be in "output/curate.db"
+python -m src.Curate.server
 ```
--- src/Curate/server.py
+++ src/Curate/server.py
@@ -1,20 +1,68 @@
 from flask import Flask, Response, send_file
-from peewee import Database
+import json
+
+from .lib.database import db, dbAlbum, dbAlbumTrack, dbArtist, dbArtistAssociation, dbTrack
+
+from peewee import JOIN
 
 app = Flask(__name__)
-db = Database('your_database_uri')
 
-@app.route('/stream')
-def stream():
-    # Retrieve the music file from the database
-    music_file = db.get('SELECT * FROM music WHERE id = 1').music_file
 
-    # Stream the music file
-    return Response(music_file, mimetype='audio/mpeg')
+@app.route('/api/artist', methods=['GET'])
+def list_artists():
+    with db.atomic() :
+        # Retrieve the list of artists from the database
+        artists = list(dbArtist.select().dicts())
 
-@app.route('/')
-def index():
-    return send_file('index.html')
+    # Return the list of artists as JSON
+    return Response(json.dumps(artists), mimetype='application/json')
+
+@app.route('/api/artist/<artist_id>', methods=['GET'])
+def get_artist_music(artist_id):
+
+    music = []
+    with db.atomic() :
+        # Retrieve the artist's music from the database
+        albums = list(dbArtistAssociation.select(dbArtistAssociation, dbAlbum).where(
+            (dbArtistAssociation.kind == 'Album') & (dbArtistAssociation.artist == artist_id)
+        ).join(dbAlbum, JOIN.LEFT_OUTER, on=(dbAlbum.id == dbArtistAssociation.item)
+        ).dicts())
+
+        if albums :
+            music.extend(albums)
+
+        singles = list(dbArtistAssociation.select(dbArtistAssociation, dbTrack.name, dbTrack.sort_name).where(
+            (dbArtistAssociation.kind == 'Track') & (dbArtistAssociation.artist == artist_id)
+        ).join(dbTrack, JOIN.LEFT_OUTER, on=(dbTrack.id == dbArtistAssociation.item)
+        ).dicts())
+
+        if singles :
+            music.extend(singles)
+
+    # Return the artist as JSON
+    return Response(json.dumps(music), mimetype='application/json')
+
+@app.route('/api/album', methods=['GET'])
+def list_albums():
+    with db.atomic() :
+        # Retrieve the list of albums from the database
+        albums = list(dbAlbum.select().dicts())
+
+    # Return the list of albums as JSON
+    return Response(json.dumps(albums), mimetype='application/json')
+
+@app.route('/api/album/<album_id>', methods=['GET'])
+def get_album_tracks(album_id):
+    with db.atomic() :
+        # Retrieve the album's tracks from the database
+        tracks = list(dbAlbumTrack.select(dbAlbumTrack, dbTrack.name, dbTrack.sort_name).where(
+            dbAlbumTrack.album == album_id
+        ).join(dbTrack, JOIN.LEFT_OUTER, on=(dbTrack.id == dbAlbumTrack.track)
+        ).order_by(dbAlbumTrack.pos).dicts())
+
+    # Return the album as JSON
+    return Response(json.dumps(tracks), mimetype='application/json')
 
 if __name__ == '__main__':
-    app.run()
+    db.init('output/curate.db', pragmas={'foreign_keys': 1})
+    app.run(port=9999, debug=True)
